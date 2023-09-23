da56a5e6cf6208dfc7b0bda08091bc1b4b1e6f68
Yadhun Sajith V V <yadhunsajith2002@gmail.com>
2023-09-23 17:26:16 +0530

    Sliver added

--- lib/main.dart
+++ lib/main.dart
@@ -7,7 +7,7 @@ void main() => runApp(const MyApp());
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
-  static const type = 1;
+  // static const type = 1;
 
   @override
   Widget build(BuildContext context) {
